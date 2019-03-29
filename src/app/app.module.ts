7f54949fd1c4a8cbe39dff19de8f460966c4821e
theerajet Chinlee <taonaja999@gmail.com>
2019-03-29 16:14:52 +0700

    add http link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  members:any=0;
+  constructor(public navCtrl: NavController, public navParam: NavParams, public http: Http){
+    this.http.get('http://localhost:8080/member')
+    .map(res=>res.json())
+    .subscribe(data=>{
+      this.members = data;
 
-  constructor(public navCtrl: NavController) {
+    });
+
+  
+   
 
   }
 
